2bcf474f3580421efb38fd64e50f67926ac9c5f3
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-12-29 15:08:26 +0100

    add powershell locals function

---
+++ src/locals/locals.ps1
@@ -0,0 +1,14 @@
+function Clonish-Locals {
+    $variables = Get-Variable
+    $expressions = New-Object System.Collections.Generic.List[System.String]
+    for ($i=0; $i -lt $variables.length; $i++) {
+        $name = $variables[$i].Name
+        $value = $variables[$i].Value
+
+        $expression = "$"
+        $expression += "$name = $value"
+        $expressions.Add($expression)
+    }
+
+    return $expressions
+}
